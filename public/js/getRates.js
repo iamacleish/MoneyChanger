45548a28f6284f17580b6f46beeec5a47838d91c
Ian MacLeish <iamacleish@gmail.com>
2016-08-05 04:16:39 -0700

    added mobile version/scaling. Added mouse over color change for < >

--- public/js/getRates.js
+++ public/js/getRates.js
@@ -6,12 +6,10 @@ var convertUSDtoEUR = function(data) {
   fx.rates = data.rates;
   var rate = fx(1).from("USD").to(fx.base);
   $("#displayRate").text("$1 = €" + rate.toFixed(4));
-  //console.log("$1 = €" + rate.toFixed(4));
 };
 
 var convertEURtoUSD = function(data) {
   fx.rates = data.rates;
   var rate = fx(1).from(fx.base).to("USD");
   $("#displayRate").text("€1 = $" + rate.toFixed(4));
-  //console.log("€1 = $" + rate.toFixed(4));
 };
