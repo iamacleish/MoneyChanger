59e50e59684b20b89b2e9bb68459539ca23f5b1c
Ian MacLeish <iamacleish@gmail.com>
2016-08-05 04:20:22 -0700

    added mobile version/scaling. Added mouse over color change for < >

--- private/js/getRates.js
+++ private/js/getRates.js
@@ -1,13 +1,16 @@
+//Gets the latest rate when the page loads.
 $(function(){
    $.getJSON("http://api.fixer.io/latest", convertUSDtoEUR);
 });
 
+//Gets USD to EUR rate and displays it on the page. fx.base is EUR.
 var convertUSDtoEUR = function(data) {
   fx.rates = data.rates;
   var rate = fx(1).from("USD").to(fx.base);
   $("#displayRate").text("$1 = €" + rate.toFixed(4));
 };
 
+//Gets EUR to USD rate and displays it on the page. fx.base is EUR.
 var convertEURtoUSD = function(data) {
   fx.rates = data.rates;
   var rate = fx(1).from(fx.base).to("USD");
